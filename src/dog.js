93d2c9f9931d5705796462dd1866c56b1fdac40b
AkihikoNambu <mate0rich.nmb@gmail.com>
2018-10-18 17:52:33 +0900

    update page1

--- src/dog.js
+++ src/dog.js
@@ -6,11 +6,11 @@ class Dog extends Animal {
 
   info() {
     this.greet();
-    console.log(chalk.cyan(`名前は${this.name}です`));
-    console.log(chalk.cyan(`犬種は${this.breed}です`));
-    console.log(chalk.cyan(`${this.age}歳です`));
+    console.log(`名前は${this.name}です`);
+    console.log(`犬種は${this.breed}です`);
+    console.log(`${this.age}歳です`);
     const humanAge = this.getHumanAge();
-    console.log(chalk.cyan(`人間年齢で${humanAge}歳です`));
+    console.log(`人間年齢で${humanAge}歳です`);
   }
 
   getHumanAge() {
